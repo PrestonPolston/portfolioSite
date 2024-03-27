5175d47d76fbda9d55699d352083283f8a4ad046
preston <polston13@outlook.com>
2024-03-27 10:16:50 -0400

    finished AboutMe page and started contact me form

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -1,5 +1,77 @@
+import Carousel from "react-material-ui-carousel";
+
+const prestonPics = [
+  {
+    label: "End of Runway Inspection",
+    imgPath: "/endOfRunway.jpeg",
+  },
+  {
+    label: "Incentive Flight",
+    imgPath: "/incentiveFlight.jpeg",
+  },
+  {
+    label: "My Jet",
+    imgPath: "/jet.jpeg",
+  },
+  {
+    label: "Reventment",
+    imgPath: "/reventments.jpeg",
+  },
+  {
+    label: "Squadron Deployment Picture",
+    imgPath: "/squadronPic.jpeg",
+  },
+  {
+    label: "Maintenance Crew Picture",
+    imgPath: "/maintPic.jpeg",
+  },
+];
+
 const AboutMe = () => {
-  return <h2>About Me page</h2>;
+  return (
+    <div>
+      <h2 style={{ textAlign: "center" }}>About Preston</h2>
+      <div style={{ width: "80%", margin: "0 auto" }}>
+        <Carousel>
+          {prestonPics.map((image) => (
+            <img
+              key={image.id}
+              src={image.imgPath}
+              alt={image.label}
+              style={{
+                height: "400px",
+                objectFit: "contain",
+                display: "block",
+                margin: "0 auto",
+              }}
+            />
+          ))}
+        </Carousel>
+      </div>
+      <p>
+        I'm Preston Polston, originally from Denison, Texas. My journey in web
+        development began when I decided to dive into a Fullstack Academy Web
+        Development Bootcamp, solidifying my skills and knowledge in HTML, CSS,
+        JavaScript, React.js, Redux.js, Node.js, npm, MySQL, PostgreSQL, GitHub,
+        Bootstrap, MUI, and Jest.
+      </p>
+      <p>
+        Before my pivot to web development, I held positions as a Rigging
+        Foreman at Entertainment Partners, managing electrical and lighting
+        rigging on film sets and locations, and a Steel Yard Manager at Metal
+        Warehouse, overseeing operations and customer service initiatives,
+        ensuring timely delivery of high-quality products.
+      </p>
+      <p>
+        With a background in the US Air Force as an F-16 Dedicated Crew Chief, I
+        gained valuable leadership skills and experience in managing teams and
+        executing operations. Merging my unique experiences, I now apply my
+        strong problem-solving abilities and leadership skills to excel in the
+        tech industry, aiming to create impactful web projects by mastering both
+        front-end and back-end technologies.
+      </p>
+    </div>
+  );
 };
 
 export default AboutMe;
--- src/components/ContactMe.jsx
+++ src/components/ContactMe.jsx
@@ -1,5 +1,60 @@
+import { useState } from "react";
+import {
+  Card,
+  CardContent,
+  Typography,
+  TextField,
+  Button,
+} from "@mui/material";
+
+const CONTACT_ENDPOINT = "??";
+
+const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
 const ContactMe = () => {
-  return <h2>Contact Me</h2>;
+  const [open, setOpen] = useState(false);
+  const [data, setData] = useState({
+    email: "",
+    content: "",
+  });
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setFormData((prevData) => ({ ...prevData, [name]: value }));
+  };
+
+  return (
+    <div>
+      <h2>Contact Me</h2>
+      <Card>
+        <CardContent>
+          <TextField
+            required
+            fullWidth
+            id="email"
+            label="Email Address"
+            name="email"
+            autoComplete="email"
+            value={data.email}
+            onChange={handleChange}
+          />
+          <TextField
+            required
+            fullWidth
+            multiline
+            rows={5}
+            id="content"
+            label="Content"
+            name="content"
+            autoComplete="content"
+            value={data.content}
+            onChange={handleChange}
+          />
+          <Button variant="contained">Submit</Button>
+        </CardContent>
+      </Card>
+    </div>
+  );
 };
 
 export default ContactMe;
